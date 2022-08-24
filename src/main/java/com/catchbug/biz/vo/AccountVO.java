d187b136aad241d7b6b2bb4f862dda9876f848cf
eunsunghoon <eunsunghoon@DESKTOP-8VQT26I>
2022-08-24 13:20:32 +0900

    계정 관련(회원가입만)

---
+++ src/main/java/com/catchbug/biz/vo/AccountVO.java
@@ -0,0 +1,30 @@
+package com.catchbug.biz.vo;
+
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+@ToString
+@Setter
+@Getter
+public class AccountVO {
+	
+	private String id; // 아이디
+	private String pass; // 비밀번호
+	private String business_no; // 사업자 등록번호
+	private String business_name; // 법인명
+	private String ceo; // 대표자
+	private String contact; // 연락처
+	private String email; // 이메일
+	private String business_address; // 사업장 주소지
+	@DateTimeFormat(pattern ="yy/mm/dd")
+	private Date regdate; // 가입일 
+	private int level1; // 등급
+	
+}
