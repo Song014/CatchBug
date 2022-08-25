1f618bf3db686e5bc9d5da7e7be71695f507cd85
eunsunghoon <eunsunghoon@DESKTOP-8VQT26I>
2022-08-25 12:31:54 +0900

    로그인 구현(등급 구분x)

--- src/main/java/com/catchbug/biz/account/MemberService.java
+++ src/main/java/com/catchbug/biz/account/MemberService.java
@@ -1,10 +1,15 @@
 package com.catchbug.biz.account;
 
+import java.util.List;
+
 import com.catchbug.biz.vo.MemberVO;
 
 public interface MemberService {
 
 	//회원 가입
 		void insertMember(MemberVO vo);
+		
+	//로그인
+		List<MemberVO> getMember(MemberVO vo);
 
 }
