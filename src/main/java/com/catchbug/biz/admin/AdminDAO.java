5b99d866c7ad9819381f38acc5d02e3037e05637
bumjin <swift08201@gmail.com>
2022-08-26 17:16:41 +0900

    가맹점 전체 리스트 추가
    기능별 주석처리

--- src/main/java/com/catchbug/biz/admin/AdminDAO.java
+++ src/main/java/com/catchbug/biz/admin/AdminDAO.java
@@ -14,16 +14,25 @@ public class AdminDAO {
 	@Autowired
 	private SqlSessionTemplate mybatis;
 	
+	// 가맹점 전체 리스트
 	public List<MemberVO> getMemberList() {
 		System.out.println("AdminDAO --> getMemberList");
-		return mybatis.selectList("AdminMapper.getList");
+		return mybatis.selectList("AdminMapper.getMemberList");
 	}
 	
+	// 가맹점 승인요청 리스트
+	public List<MemberVO> getMemberWaitList() {
+		System.out.println("AdminDAO --> getMemberList");
+		return mybatis.selectList("AdminMapper.getMemberWaitList");
+	}
+	
+	// 승인 처리
 	public void memberLevelUpdate(MemberVO vo) {
 		System.out.println("가입 승인 DAO 회원아이디 : " + vo.getId());
 		mybatis.update("AdminMapper.memberLevelUpdate", vo);
 	}
 	
+	// 반려 처리
 	public void memberrefuse(MemberVO vo) {
 		System.out.println("가입 반려 DAO 회원아이디 : " + vo.getId());
 		mybatis.delete("AdminMapper.memberrefuse", vo);
