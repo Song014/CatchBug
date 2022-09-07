75e89109b7acb98279a9c138ea357d5ea4a29d12
SONG GI SEOK <ghgsb6200@gmail.com>
2022-09-07 19:47:49 +0900

    충돌 해결

--- src/main/java/com/catchbug/biz/account/MemberServiceImpl.java
+++ src/main/java/com/catchbug/biz/account/MemberServiceImpl.java
@@ -32,18 +32,15 @@ public class MemberServiceImpl implements MemberService {
 
 	}
 
-
 	@Override
 	public List<MemberVO> getMemberList(MemberVO vo) {
 		return (List<MemberVO>) MemberDAO.getMemberList(vo);
 	}
 
-
 	@Override
 	public void updateMypage(MemberVO vo) {
 		MemberDAO.updateMypage(vo);
 	}
-
 
 	@Override
 	public void updateImg(MemberVO vo) {
@@ -55,24 +52,16 @@ public class MemberServiceImpl implements MemberService {
 	public int getTotalMember(MemberVO vo) {
 		// TODO Auto-generated method stub
 		return 0;
-
-
-	@Override
-	public void updateImg(MemberVO vo) {
-		MemberDAO.updateImg(vo);
-
 	}
-
 
 	@Override
 	public void updatePass(MemberVO vo) {
-		MemberDAO.updatePass(vo);		
+		MemberDAO.updatePass(vo);
 	}
-
 
 	@Override
 	public int idcheck(MemberVO vo) {
 		return MemberDAO.idcheck(vo);
-		
+
 	}
 }
